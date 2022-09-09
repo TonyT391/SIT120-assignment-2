a748fdf74654f4740f4edeeb7d94439d9fd1412b
TonyT391 <113053003+TonyT391@users.noreply.github.com>
2022-09-09 18:40:07 +1000

    Add files via upload

---
+++ indexW4.js
@@ -0,0 +1,11 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+      seen: true
+    }
+  })
+  
+  app.seen = true;
+
+  /* if app.seen is set to false, the message will not be displayed. */
+  
